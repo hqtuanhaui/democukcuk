06509ac617cb73acdf779a671c4a2c003c33275d
HaQuocTuan <haquoctuan0711@gmail.com>
2021-05-12 00:28:07 +0700

    validateDate

--- js/page/customer.js
+++ js/page/customer.js
@@ -13,28 +13,35 @@ function loadData() {
       console.log(data);
       // vong lap qua res
       $.each(data, function (index, item) {
+        let dateOfBirth = item["DateOfBirth"];
+        dateOfBirth = validateDate(dateOfBirth);
         let tr = $(
           `<tr>
+                
                 <td>
                   <div>` +
             item["EmployeeCode"] +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
             item["FullName"] +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
-            item["DateOfBirth"] +
+            dateOfBirth +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
             item["GenderName"] +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
             item["PhoneNumber"] +
@@ -45,16 +52,19 @@ function loadData() {
             item["Address"] +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
             item["Email"] +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
             item["Salary"] +
             `</div>
                 </td>
+                
                 <td>
                   <div>` +
             item["DepartmentName"] +
@@ -68,4 +78,19 @@ function loadData() {
       });
     })
     .fail(function (res) {});
+}
+
+// validate ngay sinh
+function validateDate(date) {
+  var date = new Date(date);
+  if (Number.isNaN(date.getTime())) {
+    return "";
+  } else {
+    var day = date.getDate(),
+      month = date.getMonth() + 1, //tháng tính từ 0
+      year = date.getFullYear();
+    day = day < 10 ? "0" + day : day;
+    month = month < 10 ? "0" + month : month;
+    return day + "/" + month + "/" + year;
+  }
 }
